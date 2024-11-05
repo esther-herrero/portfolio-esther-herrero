b3d7211d603f061b19f3eb1108a4d197f187102e
esther_herrero_ <114144024+esther-herrero@users.noreply.github.com>
2024-11-05 13:46:20 +0100

    update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,21 +9,20 @@ const Portfolio = () => {
         <>
             <section className="content-section bg-light section-margin" id="portfolio">
                 <div className="container px-4 px-lg-5">
-                    <div className="content-section-heading text-center">
+                    <div className="content-section-heading text-center portfolio-title">
                         <h2 className="text-secondary mb-3">Proyectos en React, JavaScript, TypeScript, HTML y CSS</h2>
                         <h3 className="mb-3">Portfolio: Esther R. Herrero 🍀</h3>
                         <p className="mb-5">Página en construcción. <br/> Iré subiendo pequeños proyectos poco a poco y
                             luego los iré mejorando 💪🏼.</p>
+                        <a href="https://esther-herrero.github.io/portfolio-esther-herrero/assets/CV_front_developer_Esther_Rguez_Herrero.pdf"
+                           className="btn btn-primary mb-3"
+                           target="_blank"
+                           rel="noopener noreferrer">
+                            Descargar CV
+                        </a>
                     </div>
-                    <a href="https://esther-herrero.github.io/portfolio-esther-herrero/assets/CV_front_developer_Esther_Rguez_Herrero.pdf"
-                       className="btn btn-primary mb-3"
-                       target="_blank"
-                       rel="noopener noreferrer">
-                        Descargar CV
-                    </a>
-
-
                     {/*<DownloadCV />*/}
+
                     <div className="row gx-4 gx-lg-5">
                         {projects.map((project, index) => (
                             <div className="col-lg-6 col-md-6 container-card" key={index}>
